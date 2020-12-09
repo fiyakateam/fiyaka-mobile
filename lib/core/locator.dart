9009a3a4d6b58db7112d873163099d90935ef954
xinoip <pionix@protonmail.com>
2020-12-09 18:44:27 +0300

    core: create locator for dependency injection

---
+++ lib/core/locator.dart
@@ -0,0 +1,33 @@
+import 'package:get_it/get_it.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+import 'service/device_info_service.dart';
+import 'service/dio_http_service.dart';
+import 'service/http_service.dart';
+import 'service/storage_service.dart';
+
+final locator = GetIt.instance;
+
+Future<void> initLocator() async {
+  final _httpService = DioHttpService();
+  locator.registerSingleton<HttpService>(
+    _httpService,
+  );
+
+  locator.registerLazySingleton(
+    () => NavigationService(),
+  );
+  locator.registerLazySingleton(
+    () => DialogService(),
+  );
+
+  final _deviceInfoService = await DeviceInfoService.create();
+  locator.registerSingleton(
+    _deviceInfoService,
+  );
+
+  final _storageService = await StorageService.create();
+  locator.registerSingleton(
+    _storageService,
+  );
+}
